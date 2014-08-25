d8a67eaf92443fe218f2eb3bcfc586086a8b938a
Juha Tauriainen <juha.tauriainen@appgyver.com>
2014-08-25 13:17:14 +0300

    Display average time, show list of teams for easier selection

--- js/toilet.js
+++ js/toilet.js
@@ -3,11 +3,30 @@ var app = angular.module('toilet', ['toiletModel']);
 
 app.controller('queue', function($scope, api) {
 
+  $scope.team = api.getTeam();
+
   var loadPeople = function() {
     $scope.queue = api.get();
+    var times = api.getTime();
+
+    var totalTimes = times.length;
+
+    var avg = 0;
+    for(i in times) {
+      avg += times[i];
+    }
+
+    avg /= totalTimes;
+    $scope.averageLength = moment.duration(avg, 'milliseconds').humanize();
   };
-  $scope.save = function() {
-    api.set($scope.name);
+
+  $scope.add = function(name) {
+    name = name || $scope.name;
+    if(!name) {
+      return;
+    }
+
+    api.set(name);
     loadPeople();
     $scope.name = '';
   };
@@ -47,12 +66,53 @@ module.factory('api', function() {
     }
     return JSON.parse(data);
   };
+  var getTime = function() {
+    var data = window.localStorage.getItem("time");
+    if(!data) {
+      return [];
+    }
+    return JSON.parse(data);
+  };
+  var addTime = function(time) {
+    var times = getTime();
+    times.push(time)
+    window.localStorage.setItem("time", JSON.stringify(times));
+  };
+
+  var team = [
+    {name: "JuhQ"},
+    {name: "Juha"},
+    {name: "Matti"},
+    {name: "Mevi"},
+    {name: "Eevert"},
+    {name: "Jesse"},
+    {name: "Henri"},
+    {name: "Marko"},
+    {name: "Dr. Luukkainen"},
+    {name: "Varya"},
+    {name: "Harri"},
+    {name: "Petrus"},
+    {name: "Tomi"},
+    {name: "Nate"},
+    {name: "Satu"},
+    {name: "Rafael"},
+    {name: "Sampo"},
+    {name: "Christoffer"}
+  ];
 
   return {
+    getTeam: function() {
+      return team;
+    },
     get: function() {
       var people = getPeople();
       return people;
     },
+    getTime: function() {
+      var time = getTime();
+      return time;
+    },
+    addTime: addTime,
     set: function(name) {
       var people = getPeople();
       people.push({name: name, time: new Date().getTime()});
@@ -62,9 +122,15 @@ module.factory('api', function() {
     remove: function(name) {
       var people = getPeople();
       var newList = [];
+      var timeDiff;
       for(i in people) {
         if(people[i].name !== name) {
           newList.push(people[i])
+        } else {
+          if(!timeDiff) {
+            timeDiff = new Date().getTime() - people[i].time;
+            addTime(timeDiff);
+          }
         }
       }
 
